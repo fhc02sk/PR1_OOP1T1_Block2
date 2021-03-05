7ee9ee38ff007d959b91731f7e566ebb348dfc7b
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-05 10:01:36 +0100

    DistanceDemo

--- src/DistanceDemo.java
+++ src/DistanceDemo.java
@@ -13,11 +13,21 @@ public class DistanceDemo {
         System.out.println(kilometerValue + " Kilometer entsprechen " + miles + " Meilen");
         System.out.println(17 + " Kilometer entsprechen " + miles2 + " Meilen");
         System.out.println(kilometer27 + " Kilometer entsprechen " + miles3 + " Meilen");
+
+        double kilometer12 = milesToKilometer(miles);
+        System.out.println(miles + " Meilen entsprechen " + kilometer12 + " Kilometer");
+        System.out.println(miles2 + " Meilen entsprechen " + milesToKilometer(miles2) + " Kilometer");
+        System.out.println(miles3 + " Meilen entsprechen " + milesToKilometer(miles3) + " Kilometer");
     }
 
     // Methode ist unabhängig vom Aufrufer
     public static double kilometerToMiles (double kilometer) {
-        double result = kilometer / 1.609;
+        double result = kilometer / 1.609344;
         return result; // calculated miles
+    }
+
+    public static double milesToKilometer(double miles) {
+        double result = miles * 1.609344;
+        return result;
     }
 }
